80b11f74ba491d4bd80fb230cee9ca2c9204a3cd
German Santos <santos.german@gmail.com>
2023-03-12 10:20:19 -0700

    Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee = {
+  name: "John",
+  streetAddress: "60 East 9th",
+};
+
+
+function updateEmployeeWithKeyAndValue(placeholder, key, value) {
+  const newObj = { ...placeholder };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(placeholder, key, value) {
+    //employee = { ...obj };
+  
+    placeholder[key] = value;
+  
+    return placeholder;
+  }
+
+  function deleteFromEmployeeByKey(placeholder, key) {
+    const newObj = { ...placeholder };
+    delete newObj[key];
+      
+    return newObj;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(placeholder, key) {
+    // const newObj = { ...placeholder };
+    delete placeholder[key];
+      
+    return placeholder;
+  }
